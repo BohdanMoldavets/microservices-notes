7b18f6a26b99ef3f7d066df3b1da199dd7f28cb8
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-02-16 19:12:28 +0100

    add api gateway and load balancer

--- microservices-app-example/currency-conversion-service/src/main/java/com/moldavets/microservices/currency_conversion_service/controller/CurrencyConversionController.java
+++ microservices-app-example/currency-conversion-service/src/main/java/com/moldavets/microservices/currency_conversion_service/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package com.moldavets.microservices.currency_conversion_service.controller;
 
 import com.moldavets.microservices.currency_conversion_service.entity.CurrencyConversion;
+import com.moldavets.microservices.currency_conversion_service.proxy.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +14,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("/currency-conversion")
 public class CurrencyConversionController {
+
+    private CurrencyExchangeProxy currencyExchangeProxy;
+
+    @Autowired
+    public CurrencyConversionController(CurrencyExchangeProxy currencyExchangeProxy) {
+        this.currencyExchangeProxy = currencyExchangeProxy;
+    }
 
     @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable("from") String from,
@@ -37,8 +46,29 @@ public class CurrencyConversionController {
                 quantity,
                 currencyConversion.getConversionMultiple(),
                 quantity.multiply(currencyConversion.getConversionMultiple()),
-                currencyConversion.getEnvironment()
+                currencyConversion.getEnvironment() + " rest template"
         );
     }
+
+    @GetMapping("/feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable("from") String from,
+                                                          @PathVariable("to") String to,
+                                                          @PathVariable("quantity") BigDecimal quantity) {
+
+
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from,
+                to,
+                quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " feign"
+        );
+    }
+
+
 
 }
